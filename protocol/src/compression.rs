1333640153bdbf152fd127655d9374e79f966600
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2022-08-03 18:19:47 +0300

    packet compression

---
+++ protocol/src/compression.rs
@@ -0,0 +1,44 @@
+use std::io::{Error, ErrorKind, Read, Result};
+
+use flate2::read::ZlibDecoder;
+
+use crate::packet_io::PacketReaderExt;
+
+const MAX_DATA_LENGTH: usize = 2097152;
+
+/// Reads a compressed packet.
+///
+/// Returns the packet ID and data
+pub fn read_compressed_packet(input: &mut impl Read) -> Result<(i32, Vec<u8>)> {
+    let packet_length = input.read_varint()?.0 as usize;
+    let data_length = input.read_varint()?.0 as usize;
+    if data_length != 0 {
+        let mut zlib = ZlibDecoder::new(input);
+        let packet_id = zlib.read_varint_with_size()?;
+        let mut buf = vec![0_u8; data_length - packet_id.1];
+        zlib.read_exact(buf.as_mut_slice())?;
+        Ok((packet_id.0.0, buf))
+    } else {
+        let packet_id = input.read_varint_with_size()?;
+        let data_length = packet_length - packet_id.1;
+        let mut buf = vec![0_u8; data_length - packet_id.1];
+        input.read_exact(buf.as_mut_slice())?;
+        Ok((packet_id.0.0, buf))
+    }
+}
+
+pub fn read_uncompressed_packet(input: &mut impl Read) -> Result<(i32, Vec<u8>)> {
+    let packet_length = input.read_varint()?.0 as usize;
+    let packet_id = input.read_varint_with_size()?;
+    let actual_length = packet_length - packet_id.1;
+    if actual_length < 0 || actual_length > MAX_DATA_LENGTH {
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            format!("received length {} is greater than the allowed maximum ({})", actual_length, MAX_DATA_LENGTH))
+        );
+    }
+    let mut buf = vec![0; actual_length];
+
+    input.read_exact(buf.as_mut_slice())?;
+    Ok((packet_id.0.0, buf))
+}
